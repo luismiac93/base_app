7eaf72a978fc98f070fa6caaf6a18d3378fe9eaa
Luismi <luismiac93@outlook.com>
2023-07-09 08:35:28 +1000

    Initial commit and theme configured

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:base_app/config/theme/app_theme.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme().getTheme(),
+      home: Scaffold(
+        body: Center(
+          child: Text('Hello World!'),
+        ),
+      ),
+    );
+  }
+}
